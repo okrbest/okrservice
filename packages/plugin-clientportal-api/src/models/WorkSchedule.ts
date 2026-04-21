a1dbafc65ea11016c37ebe54d799716fc7cce575
ztng123 <dpwl991102@gmail.com>
2026-04-21 23:55:24 +0900

    fix(clientportal): fix field import path and deeplinkParams type in WorkSchedule

--- packages/plugin-clientportal-api/src/models/WorkSchedule.ts
+++ packages/plugin-clientportal-api/src/models/WorkSchedule.ts
@@ -1,5 +1,4 @@
 import { Model } from 'mongoose'
 import { IWorkScheduleDocument } from './definitions/workSchedule'
 
-// 모델 생성은 connectionResolver의 db.model()을 통해 이루어짐
 export interface IWorkScheduleModel extends Model<IWorkScheduleDocument> {}
