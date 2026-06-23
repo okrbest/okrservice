2c8e89ba15780bde7bed9250a92b31cbb1367641
ztng123 <dpwl991102@gmail.com>
2026-06-23 15:16:32 +0900

    fix(kb): /knowledge-base 라우트 페이지 추가

---
+++ client-portal/pages/knowledge-base/index.tsx
@@ -0,0 +1,6 @@
+import CategoriesContainer from '../../modules/knowledgeBase/containers/CategoryList';
+import React from 'react';
+
+export default function KnowledgeBase() {
+  return <CategoriesContainer />;
+}
